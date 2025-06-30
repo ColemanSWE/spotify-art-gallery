6bbdf35ecd87f3e7544685eddbcba90b5370b29a
ColemanSWE <colemanrose.new@gmail.com>
2025-06-30 23:51:06 +0300

    Fixed type files and express config

--- packages/api/types/express.d.ts
+++ packages/api/types/express.d.ts
@@ -6,5 +6,7 @@ declare module 'express' {
     user?: {
       id: number;
     };
+    file?: Express.Multer.File;
+    files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] };
   }
 }
